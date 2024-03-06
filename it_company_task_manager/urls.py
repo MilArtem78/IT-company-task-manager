f39719cd9d4cd6e81b0cdec16095a36a4f9c75e9
Artem Milchenko <pl301078map2@gmail.com>
2024-03-06 14:09:18 +0200

    Included task_manager.urls in project root urls Configured static files

--- it_company_task_manager/urls.py
+++ it_company_task_manager/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("task_manager.urls", namespace="task_manager")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
