690cb9ca4b0bd03325884318d1f13c651185eaa8
Artem Milchenko <pl301078map2@gmail.com>
2024-03-06 13:52:09 +0200

    Added index view for the home page of the app

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from task_manager.models import Task, Worker, Position
+
+
+@login_required
+def index(request):
+
+    num_tasks = Task.objects.count()
+    num_completed_tasks = Task.objects.filter(is_completed=True).count()
+    num_workers = Worker.objects.count()
+    num_position = Position.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_tasks": num_tasks,
+        "num_workers": num_workers,
+        "num_position": num_position,
+        "num_completed_tasks": num_completed_tasks,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "task_manager/index.html", context=context)
+
