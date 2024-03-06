5f37d4eefc3042b676e1c19f11bab4eaa6d86501
Artem Milchenko <pl301078map2@gmail.com>
2024-03-06 13:33:58 +0200

    Created Worker (AbstractUser) model & Position model

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+from it_company_task_manager.settings import AUTH_USER_MODEL
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name="workers"
+    )
+
+    class Meta:
+        ordering = ["username"]
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.position})"
+
+    def get_absolute_url(self):
+        return reverse(
+            "task_manager:worker-detail",
+            kwargs={"pk": self.pk}
+        )
