291ef4a5181a7d642d6d2e7480e64604a843f981
Artem Milchenko <pl301078map2@gmail.com>
2024-03-06 13:39:41 +0200

    Registered models and customized admin panel

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from task_manager.models import Position, Worker, Task, TaskType
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "position",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Task)
+class CarAdmin(admin.ModelAdmin):
+    list_display = admin.ModelAdmin.list_display + ("is_completed",)
+    search_fields = ("name",)
+    list_filter = ("priority",)
+
+
+admin.site.register(Position)
+admin.site.register(TaskType)
+admin.site.unregister(Group)
