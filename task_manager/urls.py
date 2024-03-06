aa6b2e794044cfc7b01de7464a8ff222b077c859
Artem Milchenko <pl301078map2@gmail.com>
2024-03-06 16:07:59 +0200

    Added urls for TaskType CRUD

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,10 +2,21 @@ from django.urls import path
 
 from task_manager.views import (
     index,
+    TaskTypeListView,
+    TaskTypeCreateView,
+    TaskTypeUpdateView,
+    TaskTypeDeleteView,
+
 )
 
 urlpatterns = [
     path("", index, name="index"),
+    path("tasktypes/", TaskTypeListView.as_view(), name="task-type-list"),
+    path("tasktypes/create/", TaskTypeCreateView.as_view(), name="task-type-create"),
+    path("tasktypes/<int:pk>/update/", TaskTypeUpdateView.as_view(), name="task-type-update"),
+    path("tasktypes/<int:pk>/delete/", TaskTypeDeleteView.as_view(), name="task-type-delete"),
+
+
 ]
 
 
