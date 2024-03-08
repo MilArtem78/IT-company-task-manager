8cf695a4c7b8b3a2a0f2f3a647839700fd2f34b6
Artem Milchenko <pl301078map2@gmail.com>
2024-03-08 13:20:44 +0200

    Added urls for Position CRUD

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -6,16 +6,54 @@ from task_manager.views import (
     TaskTypeCreateView,
     TaskTypeUpdateView,
     TaskTypeDeleteView,
-
+    PositionListView,
+    PositionCreateView,
+    PositionUpdateView,
+    PositionDeleteView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
-    path("tasktypes/", TaskTypeListView.as_view(), name="task-type-list"),
-    path("tasktypes/create/", TaskTypeCreateView.as_view(), name="task-type-create"),
-    path("tasktypes/<int:pk>/update/", TaskTypeUpdateView.as_view(), name="task-type-update"),
-    path("tasktypes/<int:pk>/delete/", TaskTypeDeleteView.as_view(), name="task-type-delete"),
-
+    path(
+        "tasktypes/",
+        TaskTypeListView.as_view(),
+        name="task-type-list"
+    ),
+    path(
+        "tasktypes/create/",
+        TaskTypeCreateView.as_view(),
+        name="task-type-create"
+    ),
+    path(
+        "tasktypes/<int:pk>/update/",
+        TaskTypeUpdateView.as_view(),
+        name="task-type-update"
+    ),
+    path(
+        "tasktypes/<int:pk>/delete/",
+        TaskTypeDeleteView.as_view(),
+        name="task-type-delete"
+    ),
+    path(
+        "positions/",
+        PositionListView.as_view(),
+        name="position-list"
+    ),
+    path(
+        "positions/create/",
+        PositionCreateView.as_view(),
+        name="position-create"
+    ),
+    path(
+        "positions/<int:pk>/update/",
+        PositionUpdateView.as_view(),
+        name="position-update"
+    ),
+    path(
+        "positions/<int:pk>/delete/",
+        PositionDeleteView.as_view(),
+        name="position-delete"
+    ),
 
 ]
 
