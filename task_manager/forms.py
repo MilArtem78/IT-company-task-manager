44e85bf667d7fcc52aaedbdd63f98a03d94564a4
Artem Milchenko <pl301078map2@gmail.com>
2024-03-08 13:21:24 +0200

    Added PositionSearchForm

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -12,3 +12,16 @@ class TaskTypeSearchForm(forms.Form):
             }
         )
     )
+
+
+class PositionSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by name"
+            }
+        )
+    )
