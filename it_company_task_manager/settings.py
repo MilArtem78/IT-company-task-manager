867b185dfda8a3e0ff7dd1f7585e2717e69918da
Artem Milchenko <pl301078map2@gmail.com>
2024-03-14 15:12:07 +0200

    Added it-company-task-manager-vd82.onrender.com to ALLOWED_HOSTS

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost",]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "it-company-task-manager-vd82.onrender.com"
+]
 
 INTERNAL_IPS = [
     # ...
